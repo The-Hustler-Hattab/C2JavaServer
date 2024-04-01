9d9872824aa14f42212ca8a58dc3df2de90bac1b
Mohammed Hattab <mohammedhattab97@gmail.com>
2024-03-31 21:22:17 -0400

    changed deployment to heroku

--- deploy.ps1
+++ deploy.ps1
@@ -1,19 +1,13 @@
 # Build and package the app with Maven
 mvn clean package docker:build
 
-# Tag the Docker image
-docker tag c2-server:latest gcr.io/personal-projects-416300/c2-server:latest
+docker tag c2-server:latest registry.heroku.com/command-and-control/web
 
-# Push the Docker image to Google Container Registry (GCR)
-docker push gcr.io/personal-projects-416300/c2-server:latest
+docker push registry.heroku.com/command-and-control/web
 
-# Deploy the app to Cloud Run
-gcloud run deploy c2-server `
-  --image gcr.io/personal-projects-416300/c2-server:latest `
-  --platform managed `
-  --region us-central1 `
-  --allow-unauthenticated `
-  --cpu 1 `
-  --memory 2Gi `
-  --port=8070 `
-  --project=personal-projects-416300
+heroku container:release web -a command-and-control
+
+heroku ps:scale web=1:standard-2x  -a  command-and-control
+
+heroku open -a command-and-control
+
